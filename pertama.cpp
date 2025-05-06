2c42f5fd9ada5021e3cf47dfd1de094b2e9b5a52
Muhammad Rifqi Alimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-05-06 18:26:23 +0700

    menggunakan function void untuk input panjang dan lebar

--- pertama.cpp
+++ pertama.cpp
@@ -7,4 +7,16 @@ public:
     int panjang, lebar;
 
 public:
+    void input()
+    {
+        cout << "Masukkan panjang : ";
+        cin >> panjang;
+        cout << "Masukkan Lebar :";
+        cin >> lebar;
+    }
+
+    int luasPp(int a, int b)
+    {
+        return a * b;
+    }
 }
